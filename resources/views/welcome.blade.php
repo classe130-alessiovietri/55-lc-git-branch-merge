5dc4b95e0dddfb0451b7a74d3bfd39a4247c8cc3
Alessio Vietri <info@alessiovietri.it>
2024-10-21 12:28:46 +0200

    Aggiunto testo in home

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -6,4 +6,8 @@
 <h1 class="text-center">
     55 LC Git Branch Merge
 </h1>
+
+<p class="text-center">
+    Lorem, ipsum dolor sit amet consectetur adipisicing elit. Blanditiis, eaque assumenda quibusdam amet unde laborum vel accusamus impedit debitis error nisi, veniam repellendus cumque, dolorem perspiciatis fuga aliquam doloribus suscipit.
+</p>
 @endsection
